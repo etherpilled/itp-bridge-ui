60326b41b70a233e7230ad384d6ab73adad420a3
etherpilled | ∞ trading <admin@infinitetrading.io>
2025-02-13 18:25:42 -0400

    Update Header.tsx

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -10,7 +10,7 @@ export function Header() {
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
       <div className="flex items-start justify-between">
         <Link href="/" className="flex items-center py-2">
-          <Image src={Name} width={260} alt="" className="ml-2 mt-0.5 hidden sm:block" />
+          <Image src={Name} width={520} alt="" className="ml-2 mt-0.5 hidden sm:block" />
         </Link>
         <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-start">
           <ConnectWalletButton />

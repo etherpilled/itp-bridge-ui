de2afff0ee98e04eb05966b32eadd3ba37348318
etherpilled | ∞ trading <admin@infinitetrading.io>
2025-02-14 10:32:44 -0400

    Update Header.tsx

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,23 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { ConnectWalletButton } from '../../features/wallet/ConnectWalletButton';
-//import Logo from '../../images/logos/app-logo.svg';
 import Name from '../../images/logos/app-name.svg';
-//import Title from '../../images/logos/app-title.svg';
 
 export function Header() {
   return (
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
-      <div className="flex items-start justify-between">
-        <Link href="/" className="flex items-center py-2">
+      <div className="flex flex-col items-center justify-center">
+        <Link href="/" className="flex justify-center w-full py-2">
           <Image src={Name} width={520} alt="" className="ml-2 mt-0.5" />
         </Link>
-        <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-start">
+        <div className="mt-4">
           <ConnectWalletButton />
         </div>
       </div>
     </header>
   );
 }
-//<Image src={Logo} width={100} alt="" />
-//<Image src={Title} width={210} alt="" className="ml-2 mt-0.5 pb-px" />
